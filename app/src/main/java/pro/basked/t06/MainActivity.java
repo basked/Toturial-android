4327cbeaf758fd87e2c1944e494303cc3fbce4c2
laravel.dev <basket.baza@gmail.com>
2019-11-03 20:58:39 +0300

    07 - Send String to Another activity through Intent

--- app/src/main/java/pro/basked/t06/MainActivity.java
+++ app/src/main/java/pro/basked/t06/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void sendMessage(View view){
+        EditText editText = (EditText) findViewById(R.id.user_message) ;
+        String message = editText.getText().toString();
         Intent intent = new Intent(this,MessageActivity.class);
+        intent.putExtra("EXTRA_MESSAGE",message);
         startActivity(intent);
     }
 }
--- app/src/main/java/pro/basked/t06/MessageActivity.java
+++ app/src/main/java/pro/basked/t06/MessageActivity.java
@@ -2,7 +2,9 @@ package pro.basked.t06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("EXTRA_MESSAGE");
+        TextView textView = findViewById(R.id.display_message);
+        textView.setText(message);
     }
 }
